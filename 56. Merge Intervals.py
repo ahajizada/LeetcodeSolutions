17a7bf3f6fa93a17eed09c81d67ef7ef0c510aad
Abulfaz Hajizada <66151753+ahajizada@users.noreply.github.com>
2025-01-02 11:10:46 -0600

    Create 56. Merge Intervals.py

---
+++ 56. Merge Intervals.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda interval:interval[0])
+
+        res = []
+
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            
+            else:
+                res[-1] = [res[-1][0],max(res[-1][1],interval[1])]
+            
+        return res
